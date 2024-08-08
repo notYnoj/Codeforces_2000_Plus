008fc4efeada4f1167fa64f0ea8d011703cdad24
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-08-08 17:37:31 -0500

    Update main.cpp

--- Graphs and Trees/Wizard's Tour/main.cpp
+++ Graphs and Trees/Wizard's Tour/main.cpp
@@ -1,1 +1,9 @@
-
+/*
+brainstorming:
+this reminds me of connected components
+hes taking an even amount of roads across cities thus inside of a connected component u can use -2
+fly over
+-2 another component
+fly over...
+we cannot come back to one that has like 0 or 1
+*/
