2857b308fdffad2e12270093d7e2d902023d2902
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-08-22 21:40:04 -0500

    Update main.cpp

--- Math/Combinatorics/Domino for Young - 2000 (+ Constructive Algorithm)/main.cpp
+++ Math/Combinatorics/Domino for Young - 2000 (+ Constructive Algorithm)/main.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    /*
+      We can transform the field into a chess board like pattern followed by black and white squares
+      now we can pick all squares as long as we use black and white pattern. Theres always a way to pick black and white
+      now if we have more black than white tho we are limited by white. thus we are limited by smaller black or white
+      thus work by this and solve the problem!
+    */
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    for(ll i = 0; i<n; i++) cin>>a[i];
+    ll guarentee = 0;
+    ll w = 0; ll b = 0;
+    for(ll i = 0; i<n; i++){
+        if(i%2){
+            if(a[i]%2){
+                w++;
+            }
+        }else{
+            if(a[i]%2){
+                b++;
+            }
+        }
+        guarentee+=a[i]/2;
+    }
+    ll ans = guarentee+min(w,b);
+    cout<<ans;
+}
 
