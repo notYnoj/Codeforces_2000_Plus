4b52b9555bb2d9e4336d56056aca3b2f040b7e6e
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-11-04 13:59:37 -0600

    Update main.cpp

--- Greedy/Sakurako's Test - 2100/main.cpp
+++ Greedy/Sakurako's Test - 2100/main.cpp
@@ -11,20 +11,38 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     }
     return os;
 }
-template <typename T, std::size_t N> //custom output stream operator for array
-std::ostream& operator<<(std::ostream& os, const T (&arr)[N]) {
-    for (std::size_t i = 0; i < N; ++i) {
-        os << arr[i] << " ";
-    }
-    return os;
-}
+
 
 void solve(){
     int n,q;
     cin>>n>>q;
-    vector<int> a(n), b(q), c(n+1, 0LL);
-    for(int i = 0; i<n; i++) cin>>a[i];
-    for(int i = 0; i<q; i++) cin>>b[i];
+    vector<int> a(n), b(q), c(n+1, 0LL), ans(n+1);
+    for(int i = 0; i<n; i++) cin>>a[i], c[a[i]]++;
+    for(int i = 1; i<=n; i++){
+        c[i] += c[i-1];
+    }
+    for(int i = 1; i<=n; i++){
+        // we need to find the smallest MIDDLE number, thus there must be at least n/2 numbers to the left
+        //so we check the number of mods fr!
+        int l = 0;
+        int r = i;
+        while(l<r){
+            int mid = (l+r)/2;
+            int cnt = c[mid];
+            for(int mod_n = 1; mod_n<=mod_n*i; mod_n++){
+
+            }
+
+        }
+
+
+
+        ans[i] = l;
+    }
+
+    for(int i = 0; i<q; i++){
+        cout<<ans[b[i]]<<" ";
+    }
 
 
 }
